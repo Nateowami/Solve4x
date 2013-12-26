d16e026303fdf695e6d16ba1e0b59e038f04f7cd
Nateowami <nateowami@gmail.com>
2013-12-26 16:27:24 +0800

    Moved GUI to GUI package; GUI class; documented Solve4x.java

--- src/com/github/nateowami/solve4x/Solve4x.java
+++ src/com/github/nateowami/solve4x/Solve4x.java
@@ -18,80 +18,22 @@
 
 package com.github.nateowami.solve4x;
 
-import com.github.nateowami.solve4x.solver.Validator;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import com.github.nateowami.solve4x.gui.GUI;
 import com.github.nateowami.solve4x.Solve4x;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-
+/**
+ * Created as soon as Solve4x runs. Starts the GUI and holds a static debug method.
+ */
 public class Solve4x {
 	
 	private static final boolean DEBUG = true;
-		
+	
+	/**
+	 * Starts the program rolling by starting the GUI
+	 */
 	Solve4x(){
 		//TODO Clean GUI code
-		startGUI();
-	}
-	
-	private void startGUI() {
-		//Define GUI elements
-	    JFrame evaluateFrame = new JFrame("Evaluate Algebraic Expression");
-		JPanel mainPanel = new JPanel();
-		final JLabel lblEnterEquation = new JLabel("Enter Equation:");
-		final JTextField equationField;
-		JPanel buttonPanel = new JPanel();
-
-		//GUI layout, I don't even dare comment it.
-	    evaluateFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    evaluateFrame.getContentPane().add(mainPanel, BorderLayout.NORTH);
-	    mainPanel.setLayout(new GridLayout(3, 3, 0, 3));
-	    mainPanel.add(lblEnterEquation);
-	    
-	    equationField = new JTextField();
-	    mainPanel.add(equationField);
-	    equationField.setColumns(30);
-	    mainPanel.add(buttonPanel);
-	    
-	    //EvaluateButton lets equations evaluate.
-	    JButton btnEvaluate = new JButton("Evaluate");
-	    btnEvaluate.addActionListener(new ActionListener(){
-	    	public void actionPerformed(ActionEvent e) {
-	    		lblEnterEquation.setText("");
-	    		String equation = equationField.getText();
-	    		Boolean isValid;
-	    		
-	    		//commented out for testing
-	    		/*if(REGEX)
-	    			Validator.generateOrderOfOperations(equation);
-	    		//End testing
-	    		 * 
-	    		 */
-
-	    		//Check to see if the input was an equation or an expression
-
-	    		if(Validator.isEq(equation)){
-	    			isValid = Validator.eqIsValid(equation);
-	    		}
-	    		else{
-	    			isValid = Validator.exprIsValid(equation);
-	    		}
-
-	    		lblEnterEquation.setText("Equation Evaluation Status: "+ isValid.toString());
-	    	}
-	    });
-	    buttonPanel.add(btnEvaluate);
-	    btnEvaluate.setHorizontalAlignment(SwingConstants.LEFT);
-	    evaluateFrame.pack();
-	    evaluateFrame.setVisible(true);
+		new GUI();
 	}
 	
 	/**
---
+++ src/com/github/nateowami/solve4x/gui/GUI.java
@@ -0,0 +1,97 @@
+/*
+    Solve4x - A Java-based program to solve and explain algebra problems
+    Copyright (C) 2013 Nathaniel Paulus
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.nateowami.solve4x.gui;
+
+import com.github.nateowami.solve4x.solver.Validator;
+import com.github.nateowami.solve4x.gui.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+
+/**
+ * A temporary GUI
+ */
+public class GUI {
+
+	public GUI(){
+		//TODO Clean GUI code (possibly not necessary as it will need to be modified anyway)
+		startGUI();
+	}
+
+	private void startGUI() {
+		//Define GUI elements
+		JFrame evaluateFrame = new JFrame("Evaluate Algebraic Expression");
+		JPanel mainPanel = new JPanel();
+		final JLabel lblEnterEquation = new JLabel("Enter Equation:");
+		final JTextField equationField;
+		JPanel buttonPanel = new JPanel();
+
+		//GUI layout, I don't even dare comment it.
+		evaluateFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		evaluateFrame.getContentPane().add(mainPanel, BorderLayout.NORTH);
+		mainPanel.setLayout(new GridLayout(3, 3, 0, 3));
+		mainPanel.add(lblEnterEquation);
+
+		equationField = new JTextField();
+		mainPanel.add(equationField);
+		equationField.setColumns(30);
+		mainPanel.add(buttonPanel);
+
+		//EvaluateButton lets equations evaluate.
+		JButton btnEvaluate = new JButton("Evaluate");
+		btnEvaluate.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				lblEnterEquation.setText("");
+				String equation = equationField.getText();
+				Boolean isValid;
+
+				//commented out for testing
+				/*if(REGEX)
+    			Validator.generateOrderOfOperations(equation);
+    		//End testing
+				 * 
+				 */
+
+				//Check to see if the input was an equation or an expression
+
+				if(Validator.isEq(equation)){
+					isValid = Validator.eqIsValid(equation);
+				}
+				else{
+					isValid = Validator.exprIsValid(equation);
+				}
+
+				lblEnterEquation.setText("Equation Evaluation Status: "+ isValid.toString());
+			}
+		});
+		buttonPanel.add(btnEvaluate);
+		btnEvaluate.setHorizontalAlignment(SwingConstants.LEFT);
+		evaluateFrame.pack();
+		evaluateFrame.setVisible(true);
+	}
+}
