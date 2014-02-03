3ce48388d2f6f3f470f6c681fd537eacce41c3ab
Nateowami <nateowami@gmail.com>
2014-02-03 15:46:12 +0800

    Worked on Number.java Lots of work still

--- src/com/github/nateowami/solve4x/solver/Number.java
+++ src/com/github/nateowami/solve4x/solver/Number.java
@@ -17,6 +17,8 @@
  */
 package com.github.nateowami.solve4x.solver;
 
+import java.math.BigDecimal;
+
 /**
  * Represents a number/fraction/mixed-number such as 35, 45/6, 56&lt;36&gt;&lt;32&gt;, and 
  * provides methods for adding, subtracting, multiplying, and dividing numbers. TODO
@@ -27,9 +29,9 @@ public class Number {
 	//The numerical part of a mixed number (could be a decimal)
 	String wholeNumber;
 	//the top of a fraction
-	int top;
+	String top;
 	//and the bottom
-	int bottom;
+	String bottom;
 	
 	public Number(String num){
 		//parse the number
@@ -71,8 +73,8 @@ public class Number {
 			if(sideOfFrac.charAt(0) == '<' && sideOfFrac.charAt(sideOfFrac.length()) == '>'){
 				sideOfFrac = sideOfFrac.substring(1, sideOfFrac.length()-1);
 			}
-			if(i == 0) top = Integer.parseInt(sideOfFrac);
-			else bottom = Integer.parseInt(sideOfFrac);
+			if(i == 0) top = sideOfFrac;
+			else bottom = sideOfFrac;
 		}
 	}
 
@@ -84,17 +86,89 @@ public class Number {
 	}
 
 	/**
-	 * @return the top of the fraction
+	 * @return the numerator of the fraction
 	 */
-	public int getTop() {
+	public String getTop() {
 		return top;
 	}
 
 	/**
-	 * @return the bottom of the fraction
+	 * @return the denominator of the fraction
 	 */
-	public int getBottom() {
+	public String getBottom() {
 		return bottom;
+	}
+	
+	 /**
+	 * Adds two numbers and returns the result
+	 * @param n1 The first Number
+	 * @param n2 The second Number
+	 * @return A Number equal to the value of the two numbers added
+	 * @throws IllegalArgumentException If the denominators of the fractions of the two numbers 
+	 * (if they have fractions, that is) are not equal
+	 */
+	public Number add(Number n1, Number n2) throws IllegalArgumentException{
+		
+		
+		return new Number("");
+	}
+	
+	/**
+	 * Adds two numbers represented by a String. This should not be confused with adding two Numbers;
+	 * this only adds integer/decimal values represented by a string
+	 * @param n1 The first number to add
+	 * @param n2 The second number to add
+	 * @return The value of two numbers added
+	 * @throws IllegalArgumentException If the Strings cannot be parsed as ints or decimals
+	 */
+	private String add(String n1, String n2) throws IllegalArgumentException{
+		//first see if they are both ints
+		if(Util.isInteger(n1) && Util.isInteger(n2)){
+			//since they can be parsed as ints just convert to ints, add, and convert to String
+			return (Integer.parseInt(n1) + Integer.parseInt(n2)) + "";
+		}
+		//they're not ints then.
+		//First make the decimal places line up.
+		//we do this by finding the first instance of a decimal in each
+		//whichever one has the decimal earlier needs zeros put at the beginning of it to align the decimal places
+		//set where the decimal place belongs
+		int index1 = n1.indexOf('.') == -1 ? n1.length() : n1.indexOf('.');
+		int index2 = n2.indexOf('.') == -1 ? n2.length() : n2.indexOf('.');
+		
+		//now insert the decimals if they're not already there
+		if(index1 == n1.length()){
+			//There is no decimal at the end. Add it.
+			n1 = n1 + ".";
+		}
+		if(index2 == n2.length()){
+			//There is no decimal at the end. Add it.
+			n2 = n2 + ".";
+		}
+		//Now they both have decimals. We can use index1 and index2 to figure out where they are.
+		if(index1 > index2){
+			//the numbers look something like this: 23.56
+			//                                      2.04
+			//so we need to add (index1 - index2) # of zeros to the beginning of n2
+			for(int i = 0; i < index1 - index2; i++){
+				//add index1 - inex2 # of zeros to the beginning of n2
+				n2 = "0" + n2;
+			}
+		}
+		//now do the reverse if index1 < index2
+		else if(index1 < index2){
+			//the numbers look something like this: 2.56
+			//                                      23.04
+			//so we need to add (index2 - index1) # of zeros to the beginning of n1
+			for(int i = 0; i < index2 - index1; i++){
+				//add index1 - inex2 # of zeros to the beginning of n2
+				n1 = "0" + n1;
+			}
+		}
+		//now we know the numbers look something like this: 23.576
+		//                                                  04.57
+		//we still need to add zeros to the end of whichever one is shorter
+		//TODO
+		return null;
 	}
 	
 }
